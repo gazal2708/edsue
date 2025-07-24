000565cbf7e7a456bd68b30a155bc90a80166b2a
gazal2708 <your-email@example.com>
2025-07-24 13:32:31 +0530

    removed ext config url

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -84,7 +84,7 @@ export function decorateMain(main) {
   buildAutoBlocks(main);
   decorateSections(main);
   decorateBlocks(main);
-  decorateExternalImages(main);
+  // decorateExternalImages(main);
 }
 
 /**
